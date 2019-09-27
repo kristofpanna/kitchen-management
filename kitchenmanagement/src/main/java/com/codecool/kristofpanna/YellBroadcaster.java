8b49d7c542073604204d2728b8d337402cd19ab7
Panna Kristóf <kristofpanna@gmail.com>
2019-09-28 00:36:45 +0200

    Refactor: use Function functional interface instead of the custom one
    (cherry picked from commit 6c6b8f7461be406b9b89eacbd8aa3d3756a194c2)

--- kitchenmanagement/src/main/java/com/codecool/kristofpanna/YellBroadcaster.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.kristofpanna;
-
-import com.codecool.kristofpanna.ingredients.IngredientType;
-
-@FunctionalInterface
-public interface YellBroadcaster {
-    boolean askForIngredient(IngredientType ingredientType);
-}
