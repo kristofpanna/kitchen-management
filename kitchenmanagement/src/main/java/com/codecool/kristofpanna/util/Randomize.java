f41299fd07f48e2b0fc5cdedfef71ae5e4f1a887
Panna Kristóf <kristofpanna@gmail.com>
2019-09-27 16:40:19 +0200

    Add randomization of an event

--- kitchenmanagement/src/main/java/com/codecool/kristofpanna/util/Randomize.java
+++ kitchenmanagement/src/main/java/com/codecool/kristofpanna/util/Randomize.java
@@ -5,8 +5,23 @@ import java.util.Random;
 public class Randomize {
     private static final Random random = new Random();
 
+    /**
+     * Get a random integer between 1 and the given number.
+     * @param to max possible value, inclusive
+     */
     public static int getRandInt(int to) {
         return random.nextInt(to + 1);
     }
+
+    /**
+     * Get random event with the given probability.
+     * @param probabilityPercent probability of true in percent
+     * @return if a random event happened or not
+     */
+    public static boolean isRandomEvent(int probabilityPercent) {
+        return random.nextInt(100) < probabilityPercent;
+    }
+
+
 
 }
